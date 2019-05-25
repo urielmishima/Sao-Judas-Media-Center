d42305dbbdb4ff29beb31a5be7fe6ca8b4f19094
Uriel Mishima <uriel.hmfm@gmail.com>
2019-05-25 15:04:14 -0300

    Salvando para rollback antes de fazer mudanças grandes

--- src/main/java/br/usjt/saojudasmediacenter/controller/PageController.java
+++ src/main/java/br/usjt/saojudasmediacenter/controller/PageController.java
@@ -1,14 +1,26 @@
 package br.usjt.saojudasmediacenter.controller;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class PageController {
+public class PageController implements ErrorController{
 	
 	@GetMapping("/")
 	public String home() {
 		return "redirect:/conteudos/timeline";
+	}
+	
+	@RequestMapping("/error")
+	public String handleError() {
+		return "error";
+	}
+
+	@Override
+	public String getErrorPath() {
+		return "/error";
 	}
 
 }
